0dde3fddb7a291082ad782c7cf743f298613e9c7
こもれび / popkechupki <hobbypopkechupki@gmail.com>
2020-04-06 00:28:03 +0900

    すでに資源ワールドが存在する時に新ワールドが生成されない問題を修正。(v1.1)

--- src/main/java/net/comorevi/plugin/worldgenerator/WorldGenerator.java
+++ src/main/java/net/comorevi/plugin/worldgenerator/WorldGenerator.java
@@ -3,6 +3,7 @@ package net.comorevi.plugin.worldgenerator;
 import cn.nukkit.level.generator.Generator;
 import cn.nukkit.plugin.PluginBase;
 import net.comorevi.plugin.cosseworldgenerator.CoSSeWorldGenerator;
+import org.iq80.leveldb.util.FileUtils;
 
 import java.io.File;
 import java.util.Calendar;
@@ -14,7 +15,7 @@ public class WorldGenerator extends PluginBase {
     @Override
     public void onEnable() {
         Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("Asia/Tokyo"));
-        File dir = new File(this.getServer().getFilePath() + File.separator + "worlds" + File.separator + "resource");
+        File dir = new File("./worlds/resource");
         if (calendar.get(Calendar.HOUR_OF_DAY) != 0 && dir.exists()) {
             loadLevel();
             return;
@@ -46,6 +47,10 @@ public class WorldGenerator extends PluginBase {
     }
 
     private void generateLevel(int type) {
+        if (new File("./worlds/resource").exists()) {
+            if (getServer().getLevelByName("resource") != null) getServer().getLevelByName("resource").unload(true);
+            FileUtils.deleteDirectoryContents(new File("./worlds/resource"));
+        }
         Long seed = new Random().nextLong();
         Class<? extends Generator> generator = Generator.getGenerator(type);
         getServer().generateLevel("resource", seed, generator);
